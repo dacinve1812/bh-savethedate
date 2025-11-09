a9dee89b737621cf0f9f5359301e8e8ea8ebff88
dacinve1812 <vuongchibao1097@gmail.com>
2025-11-09 00:04:12 -0600

    test deloy

--- src/HeroShowcase.jsx
+++ src/HeroShowcase.jsx
@@ -1,24 +1,15 @@
 import React from "react";
-import ticketImg from "../public/ticket.png";
-import photoOneImg from "../public/NP__7180.JPG";
-import photoTwoImg from "../public/NP__7045.jpg";
-import envelopeImg from "../public/envelope_1.png"; 
-import envelopeOpenImg from "../public/envelope_open.png";  
-import flowerImg from "../public/flower.png";
-import flowerImg2 from "../public/flower2.png";
-import flowerImg3 from "../public/flower3.png";
-import flowerImg4 from "../public/flower4.png";
 
 const heroMedia = {
-  photoOne: photoOneImg,
-  photoTwo: photoTwoImg,
-  card: ticketImg,
-  envelope: envelopeImg,
-  envelope_open: envelopeOpenImg,
-  flowers: flowerImg,
-  flowers_2: flowerImg2,
-  flowers_3: flowerImg3,
-  flowers_4: flowerImg4,
+  photoOne: "/NP__7180.JPG",
+  photoTwo: "/NP__7045.JPG",
+  card: "/ticket.png",
+  envelope: "/envelope_1.png",
+  envelopeOpen: "/envelope_open.png",
+  flowers: "/flower.png",
+  flowersAlt: "/flower2.png",
+  flowersAlt2: "/flower3.png",
+  flowersAlt3: "/flower4.png",
 };
 
 export default function HeroShowcase() {
@@ -41,14 +32,14 @@ export default function HeroShowcase() {
           <img src={heroMedia.envelope} alt="Invitation envelope" />
         </div>
         <div className="hero-layer hero-delay-2 hero-envelope hero-z-6">
-          <img src={heroMedia.envelope_open} alt="Invitation envelope" />
+          <img src={heroMedia.envelopeOpen} alt="Invitation envelope open" />
         </div>
 
         <div className="hero-layer hero-delay-4 hero-flower hero-flower--left hero-z-5">
           <img src={heroMedia.flowers} alt="Bloom arrangement left" />
         </div>
         <div className="hero-layer hero-delay-4 hero-flower hero-flower--left-2 hero-z-1">
-          <img src={heroMedia.flowers_2} alt="Bloom arrangement left" />
+          <img src={heroMedia.flowersAlt} alt="Bloom arrangement left alt" />
         </div>
 
         <div className="hero-layer hero-delay-4 hero-flower hero-flower--right hero-z-3">
@@ -60,13 +51,12 @@ export default function HeroShowcase() {
         </div>
 
         <div className="hero-layer hero-delay-4 hero-flower hero-flower--center-2 hero-z-6">
-          <img src={heroMedia.flowers_2} alt="Bloom arrangement center" />
+          <img src={heroMedia.flowersAlt} alt="Bloom arrangement center alt" />
         </div>
         <div className="hero-layer hero-delay-4 hero-flower hero-flower--center-3 hero-z-6">
-          <img src={heroMedia.flowers_3} alt="Bloom arrangement center" />
+          <img src={heroMedia.flowersAlt2} alt="Bloom arrangement center alt" />
         </div>
       </div>
     </section>
   );
 }
-
